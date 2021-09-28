dc88e769719e263bde7b9d3f3f76690458df829a
Muhammed Shittu <coolzyte@gmail.com>
2021-09-28 17:36:45 +0100

    toolbar and navmenu completed

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,30 @@
+import "./topbar.scss";
+import { Person, Mail} from '@material-ui/icons';
+
+export default function Topbar({menuOpen, setMenuOpen}) {
+    return (
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+            <div className="left">
+                <a href="#intro" className="logo">mst.</a>
+                <div className="itemContainer">
+                    <Person className="icon"/>
+                    <span>+234 8144 18900 88</span>
+                </div>
+                <div className="itemContainer">
+                    <Mail className="icon"/>
+                    <span>coolzyte@gmail.com</span>
+                </div>
+            </div>
+            <div className="right">
+                <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                    <span className="line1"></span>
+                    <span className="line2"></span>
+                    <span className="line3"></span>
+                </div>
+            </div>
+
+            </div>
+        </div>
+    )
+}
